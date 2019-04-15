19fa168638d3bd1b6ac5887eb29ac360a59e9092
Gerad Ryan <Gerardryan1989@gmail.com>
2019-04-15 04:08:52 +0100

    commit and push test

--- src/main/java/edu/cpp/cs580/util/ResourceResolver.java
+++ src/main/java/edu/cpp/cs580/util/ResourceResolver.java
@@ -6,7 +6,7 @@ import java.io.File;
  * This is an utility class to help file locating.
  */
 public class ResourceResolver {
-
+//comment
 	/** The base folder to store all the data used by this project. */
 	private static final String BASE_DIR = System.getProperty("user.home") + "/cs580";
 
