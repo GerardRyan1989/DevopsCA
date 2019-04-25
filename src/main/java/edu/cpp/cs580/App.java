9f03ee57216f834364ce379ce77fb3c551069ab4
Gerad Ryan <Gerardryan1989@gmail.com>
2019-04-25 02:51:17 +0100

    added sentry

--- src/main/java/edu/cpp/cs580/App.java
+++ src/main/java/edu/cpp/cs580/App.java
@@ -1,5 +1,8 @@
 package edu.cpp.cs580;
 
+import io.sentry.Sentry;
+import io.sentry.SentryClient;
+import io.sentry.SentryClientFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,18 +16,24 @@ import edu.cpp.cs580.data.provider.FSUserManager;
 import edu.cpp.cs580.data.provider.GpsProductManager;
 import edu.cpp.cs580.data.provider.UserManager;
 
+import io.sentry.context.Context;
+import io.sentry.event.BreadcrumbBuilder;
+import io.sentry.event.UserBuilder;
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class App {
     //kansdkgpfn
 	private static final Logger logger = LoggerFactory.getLogger(App.class.getName());
+    private static SentryClient sentry;
     /**
      * This is a good example of how Spring instantiates
      * objects. The instances generated from this method
      * will be used in this project, where the Autowired
      * annotation is applied.
      */
+
     @Bean
     public UserManager userManager() {
         UserManager userManager =  new FSUserManager();
@@ -47,5 +56,46 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Run Spring Boot
         SpringApplication.run(App.class, args);
+        sentry = SentryClientFactory.sentryClient();
+        logWithStaticAPI();
+    }
+
+    private static void unsafeMethod() {
+        throw new UnsupportedOperationException("You shouldn't call this!");
+    }
+
+    private static void logWithStaticAPI() {
+        // Note that all fields set on the context are optional. Context data is copied onto
+        // all future events in the current context (until the context is cleared).
+
+        // Record a breadcrumb in the current context. By default the last 100 breadcrumbs are kept.
+        Sentry.getContext().recordBreadcrumb(
+                new BreadcrumbBuilder().setMessage("User made an action").build()
+        );
+
+        // Set the user in the current context.
+        Sentry.getContext().setUser(
+                new UserBuilder().setEmail("hello@sentry.io").build()
+        );
+
+        // Add extra data to future events in this context.
+        Sentry.getContext().addExtra("extra", "thing");
+
+        // Add an additional tag to future events in this context.
+        Sentry.getContext().addTag("tagName", "tagValue");
+
+        /*
+         This sends a simple event to Sentry using the statically stored instance
+         that was created in the ``main`` method.
+         */
+        Sentry.capture("This is a test");
+
+        try {
+            unsafeMethod();
+        } catch (Exception e) {
+            // This sends an exception event to Sentry using the statically stored instance
+            // that was created in the ``main`` method.
+            Sentry.capture(e);
+        }
     }
 }
